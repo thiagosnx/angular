6bed5c063aab46e70fe407aeedc176e25977cfe9
thiagosnx <thiagosantanacq@gmail.com>
2023-12-05 17:59:28 -0300

    interfaces

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Animal } from '../../Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrl: './list-render.component.css'
 })
 export class ListRenderComponent {
-  animals = [
-    { name: 'Bob', type: 'Dog'},
-    { name: 'Marley', type: 'Dog'},
-    { name: 'Calabreso', type: 'Tiger'},
+  animals: Animal[] = [
+    { name: 'Bob', type: 'Dog', age: 4},
+    { name: 'Marley', type: 'Dog', age: 3},
+    { name: 'Calabreso', type: 'Tiger', age: 10},
   ];
+
+  animalDetails = '';
+
+  showAge(animal: Animal): void {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`
+  }
 
 }
