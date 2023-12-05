89165bd3ad60ae20b28d54fbf9038165640ae16d
thiagosnx <thiagosantanacq@gmail.com>
2023-12-05 18:06:28 -0300

    pipes

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.css'
+})
+export class PipesComponent {
+
+  someText = "Alguma COISA TAMBEM";
+
+  today = new Date();
+
+}
