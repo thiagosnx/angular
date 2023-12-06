c253b9b8d626ef18520575ad5e5090fcfa8c0ab1
thiagosnx <thiagosantanacq@gmail.com>
2023-12-06 14:55:54 -0300

    services

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { Animal } from '../../Animal';
 
+import { ListService } from '../../services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -16,8 +18,15 @@ export class ListRenderComponent {
 
   animalDetails = '';
 
+  constructor(private listService: ListService){}
+
   showAge(animal: Animal): void {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos`
+  }
+
+  removeAnimal(animal: Animal){
+    console.log("ok")
+    this.animals = this.listService.remove(this.animals,animal);
   }
 
 }
